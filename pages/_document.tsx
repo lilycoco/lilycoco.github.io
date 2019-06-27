28afdff69250e87380311491501ff6b7164e237e
lilycoco <ry.sep27@gmail.com>
2019-06-27 23:17:39 +0900

    add bootstrap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,10 @@ export default class MyDocument extends Document {
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width,initial-scale=1' />
           <meta name='author' content='Lilycoco' />
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'
+          />
         </Head>
         <body>
           <Main />
