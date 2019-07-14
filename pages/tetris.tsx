de596beb7a5ade1432c0a9932768de6e45399208
川島遼子 <ryoko@kawashimaryoukos-MacBook-Pro.local>
2019-07-15 01:28:38 +0900

    add styled-jsx

--- pages/tetris.tsx
+++ pages/tetris.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import { Layout } from '../components/Layout'
 import { BrockShape, BoardType, BrockColors } from '../components/TetrisComponent'
+import tetrisStyles from '../styled/Tetris'
 
 const boardStyle = (defaultBoard: number[][]) => {
   return defaultBoard.map((line: number[], colNo: number) => (
@@ -14,11 +15,12 @@ const boardStyle = (defaultBoard: number[][]) => {
           {num}
         </div>
       ))}
+      <style jsx>{tetrisStyles}</style>
     </div>
   ))
 }
 
-function drowNewBoard(
+function drowBoard(
   defaultBoard: number[][],
   x: number,
   y: number,
@@ -26,10 +28,10 @@ function drowNewBoard(
   currentShape: number,
   running: boolean,
 ) {
-  let dummy = defaultBoard
+  let newBoard = defaultBoard
   if (running) {
     console.log(currentShape)
-    dummy = defaultBoard.map((line: number[], lineIndex: number) =>
+    newBoard = defaultBoard.map((line: number[], lineIndex: number) =>
       line.map((block: number, blockIndex: number) => {
         const currentBlock = BrockShape[currentShape]
         if (
@@ -46,8 +48,7 @@ function drowNewBoard(
       }),
     )
   }
-  console.log(dummy)
-  return boardStyle(dummy)
+  return newBoard
 }
 
 function Game() {
@@ -56,9 +57,9 @@ function Game() {
   const [currentColor, setCurrentColor] = useState(selectColor)
   const [currentShape, setCurrentShape] = useState(selectShape)
   const [running, setRunning] = useState(false)
-  const [board, setBoard] = useState(BoardType)
   const [x, setX] = useState(4)
   const [y, setY] = useState(-BrockShape[currentShape].length)
+  const [board, setBoard] = useState(BoardType)
 
   console.log(currentShape)
   const downHandler = ({ key }: any) => {
@@ -81,6 +82,11 @@ function Game() {
   useEffect(() => {
     window.addEventListener('keydown', downHandler)
     const flowBlock: any = setInterval(() => {
+      // if(!(y + BrockShape[currentShape].length >= 20)) {
+      //   setCurrentShape(selectShape)
+      //   setCurrentColor(selectColor)
+      //   setBoard(drowBoard(board, x, y, currentColor, currentShape, running))
+      // }
       setY((y) => (y + BrockShape[currentShape].length < 20 ? y + 1 : 0))
     }, 1000)
     return () => {
@@ -89,16 +95,15 @@ function Game() {
     }
   }, [])
 
-  const drowBoard = boardStyle(board)
-  const newboard = drowNewBoard(BoardType, x, y, currentColor, currentShape, running)
-
+  // const baseBoard = boardStyle(board)
+  const newboard = boardStyle(drowBoard(BoardType, x, y, currentColor, currentShape, running))
   const handleRunClick = () => setRunning(!running)
   const handleClearClick = () => {}
 
   return (
     <div>
       <div className='boardWrapper'>
-        {/* <div className='board'>{drowBoard}</div> */}
+        {/* <div className='board'>{baseBoard}</div> */}
         <div className='newBoard'>{newboard}</div>
       </div>
       <button className='btn btn-primary' onClick={() => handleRunClick()}>
@@ -108,44 +113,7 @@ function Game() {
         Clear
       </button>
       <label style={{ fontSize: '5em', display: 'block' }}>{y}</label>
-      <style>
-        {`
-        .boardWrapper{
-          position: relative;
-          width: 250px;
-          background-color: rgb(15, 15, 27);
-          height: 600px;
-        }
-        .newBoard, .board{
-          position: absolute;
-          width: 100%;
-          top: 0px;
-          left: 0px;
-        }
-        .line {
-          display: flex;
-        }
-        .block {
-          width: 30px;
-          height: 30px;
-          box-sizing: border-box;
-          border: 1px solid rgba(0, 0, 0, 0.253);
-          border-collapse: collapse;
-          text-align: center;
-          box-sizing: border-box;
-          color: white;
-        }
-        .clear {
-          backgroud-color: rgb(254, 254, 254, 0);
-        }
-        .color0, .color1, .color2, .color3, .color4, .color5{
-          border:5px outset rgba(255, 255, 255, 0.568);
-        }
-        .btn {
-          margin-top: 30px;
-        }
-        `}
-      </style>
+      <style jsx>{tetrisStyles}</style>
     </div>
   )
 }
