89f674f5ae8b39ce448debfd297f1c0306d99c5a
川島遼子 <ryoko@kawashimaryoukos-MacBook-Pro.local>
2019-07-16 01:35:22 +0900

    restore css

--- pages/tetris.tsx
+++ pages/tetris.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import { Layout } from '../components/Layout'
 import { BrockShape, BoardType, BrockColors } from '../components/TetrisComponent'
-import tetrisStyles from '../styled/Tetris'
 
 const boardStyle = (defaultBoard: number[][]) => {
   return defaultBoard.map((line: number[], colNo: number) => (
@@ -15,7 +14,6 @@ const boardStyle = (defaultBoard: number[][]) => {
           {num}
         </div>
       ))}
-      <style jsx>{tetrisStyles}</style>
     </div>
   ))
 }
@@ -111,7 +109,50 @@ function Game() {
         Clear
       </button>
       <label style={{ fontSize: '5em', display: 'block' }}>{y}</label>
-      <style jsx>{tetrisStyles}</style>
+      <style>
+        {`
+        .boardWrapper {
+          position: relative;
+          width: 250px;
+          background-color: rgb(15, 15, 27);
+          height: 600px;
+        }
+        .newBoard,
+        .board {
+          position: absolute;
+          width: 100%;
+          top: 0px;
+          left: 0px;
+        }
+        .line {
+          display: flex;
+        }
+        .block {
+          width: 30px;
+          height: 30px;
+          border: 1px solid rgba(0, 0, 0, 0.253);
+          border-collapse: collapse;
+          text-align: center;
+          box-sizing: border-box;
+          color: white;
+          line-height: 30px;
+        }
+        .clear {
+          background-color: rgb(254, 254, 254, 0);
+        }
+        .color0,
+        .color1,
+        .color2,
+        .color3,
+        .color4,
+        .color5 {
+          border: 5px outset rgba(255, 255, 255, 0.568);
+        }
+        .btn {
+          margin-top: 30px;
+        }
+      `}
+      </style>
     </div>
   )
 }
