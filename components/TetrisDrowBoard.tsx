3804fb74eaecb31b18487c932c9937d6c923f933
川島遼子 <ryoko@kawashimaryoukos-MacBook-Pro.local>
2019-07-18 00:40:41 +0900

    add directory lib

--- components/TetrisDrowBoard.tsx
+++ components/TetrisDrowBoard.tsx
@@ -25,7 +25,7 @@ export const DrowBoard = (defaultBoard: number[][]) =>
   defaultBoard.map((line: number[], colNo: number) => (
     <div key={colNo} style={{ display: 'flex' }}>
       {line.map((num: number, rowNo: number) => (
-        <Block key={rowNo} color={num}></Block>
+        <Block key={rowNo} color={num} />
       ))}
     </div>
   ))
