85e29d51dae00dd75445ad18b5aecc47b71bea6c
川島遼子 <ryoko@kawashimaryoukos-MacBook-Pro.local>
2019-07-17 14:26:09 +0900

    refactoring

--- components/TetrisDrowBoard.tsx
+++ components/TetrisDrowBoard.tsx
@@ -1,39 +1,35 @@
 export const brockColors = ['navy', 'darkmagenta', 'orangered', 'yellow', 'deeppink', 'limegreen']
+import styled from 'styled-components'
+
+export const Block = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid rgba(0, 0, 0, 0.253);
+  border-collapse: collapse;
+  text-align: center;
+  box-sizing: border-box;
+  color: white;
+  line-height: 30px;
+`
 
 export const DrowBoard = (defaultBoard: number[][]) => {
   return defaultBoard.map((line: number[], colNo: number) => (
     <div key={colNo} style={{ display: 'flex' }}>
       {line.map((num: number, rowNo: number) => (
-        <div
+        <Block
           key={rowNo}
-          className={'block ' + (!num && 'clear')}
           style={
             num
               ? {
                   backgroundColor: brockColors[num - 1],
                   border: '5px outset rgba(255, 255, 255, 0.568)',
                 }
-              : undefined
+              : {
+                  backgroundColor: 'rgb(254, 254, 254, 0)',
+                }
           }
-        ></div>
+        ></Block>
       ))}
-      <style>
-        {`
-        .block {
-          width: 30px;
-          height: 30px;
-          border: 1px solid rgba(0, 0, 0, 0.253);
-          border-collapse: collapse;
-          text-align: center;
-          box-sizing: border-box;
-          color: white;
-          line-height: 30px;
-        }
-        .clear {
-          background-color: rgb(254, 254, 254, 0);
-        }
-      `}
-      </style>
     </div>
   ))
 }
