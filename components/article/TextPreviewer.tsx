e3dc682721fcb003411dd27c53bb94a512ce5ae9
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-02 13:15:06 +0900

    add mdx

--- components/article/TextPreviewer.tsx
+++ components/article/TextPreviewer.tsx
@@ -1,34 +1,14 @@
 import * as React from 'react'
-import ReactMarkdown from 'react-markdown'
 import { withRouter } from 'next/router'
-import { title, text } from '../../text/blog_1'
+import Readme from '../../text/text.md'
 
-export const TextPreviewer = withRouter((props: { router: { query: { id: string } } }) => (
-  <div>
-    <h1>
-      {props.router.query.id}
-      {title}
-    </h1>
-    <div className='markdown'>
-      <ReactMarkdown source={text} />
+export const TextPreviewer = withRouter((props: { router: { query: { id: string } } }) => {
+  return (
+    <div>
+      <h1>{props.router.query.id}</h1>
+      <div className='markdown'>
+        <Readme />
+      </div>
     </div>
-    <style>{`
-      .markdown {
-        font-family: 'Arial';
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-        a:hover {
-          opacity: 0.6;
-        }
-        h3 {
-          margin: 0;
-          padding: 0;
-          text-transform: uppercase;
-        }
-      }
-      
-    `}</style>
-  </div>
-))
+  )
+})
--- next.config.js
+++ next.config.js
@@ -1,18 +1,27 @@
+/* eslint-disable prettier/prettier */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript({
-  poweredByHeader: false,
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-      '/home': { page: '/home' },
-      '/blog': { page: '/blog' },
-      '/tetris': { page: '/tetris' },
-      '/tictac': { page: '/tictac' },
-      '/blog/1': { page: '/article', query: { id: 1 } },
-      '/blog/2': { page: '/article', query: { id: 2 } },
-      '/blog/3': { page: '/article', query: { id: 3 } },
-    }
-  },
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)$/,
 })
+
+module.exports = withMDX(
+  withTypescript({
+    poweredByHeader: false,
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' },
+        '/home': { page: '/home' },
+        '/blog': { page: '/blog' },
+        '/tetris': { page: '/tetris' },
+        '/tictac': { page: '/tictac' },
+        '/blog/1': { page: '/article', query: { id: 1 } },
+        '/blog/2': { page: '/article', query: { id: 2 } },
+        '/blog/3': { page: '/article', query: { id: 3 } },
+      }
+    },
+  })
+);
+
