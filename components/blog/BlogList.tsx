449a55d225b626cdba6aa255396c031d422fc5ee
Ryoko Kawashima <ryoko@Ryokos-MacBook-Pro.local>
2019-08-04 23:01:41 +0900

    add blog conponent

--- components/blog/BlogList.tsx
+++ components/blog/BlogList.tsx
@@ -2,44 +2,29 @@ import { MainContents } from '../Style'
 import Link from 'next/link'
 import moment from 'moment'
 
-const createPost = (post: any, prev: any = null) => {
+const BlogLink = (props: { post: any; prev: any }) => {
+  const { post, prev } = props
   const year = post.date.substr(0, 4)
   const prevYear = prev ? prev.date.substr(0, 4) : null
   return (
-    <div key={post.href}>
+    <li>
       {(() => (year !== prevYear ? <h3 className='code'>{year}</h3> : null))()}
-      <ul>
-        <li>
-          <div className='post-date code'>{moment(post.date).format('MMM DD')}</div>
-          <div className='title'>
-            <Link as={'/blog/' + post.href} href={`/article?id=${post.href}`}>
-              <a>{post.title}</a>
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </div>
+      <div className='post-date code'>{moment(post.date).format('MMM DD')}</div>
+      <div className='title'>
+        <Link as={'/blog/' + post.href} href={`/article?id=${post.href}`}>
+          <a>{post.title}</a>
+        </Link>
+      </div>
+    </li>
   )
 }
 
 export const BlogList = (props: any) => (
   <MainContents>
-    <section>
-      {props.posts.map((post: any, i: number) => createPost(post, props.posts[i - 1]))}
-      <style jsx>{`
-        ul,
-        ol {
-          margin: 40px 0;
-          padding-left: 50px;
-        }
-        ul li {
-          word-wrap: break-word;
-        }
-        ul img {
-          margin: 40px 0;
-          border-radius: 5px;
-        }
-      `}</style>
-    </section>
+    <ul>
+      {props.posts.map((post: any, i: number) => (
+        <BlogLink post={post} prev={props.posts[i - 1]} key={post.href} />
+      ))}
+    </ul>
   </MainContents>
 )
