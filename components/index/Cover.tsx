e69cdb9aa0cba8081dfda3e15d652e9bfa90b53c
Ryoko Kawashima <ry.sep27@gmail.com>
2021-02-26 02:04:21 +0900

    fix style

--- components/index/Cover.tsx
+++ components/index/Cover.tsx
@@ -2,26 +2,13 @@ import { JumboTron, CoverImage, JumboTronHeading, JumboTronContainer } from './S
 
 export const Cover = () => (
   <CoverImage>
-    <JumboTron className='jumbotron text-center'>
+    <JumboTron className='text-center'>
       <JumboTronContainer className='container'>
         <JumboTronHeading className='jumbotron-heading'>Welcome to my page!</JumboTronHeading>
         <p className='lead'>
-          This is Lilycoco's website, who is a React.js, Node.js and TypeScript developer.
+          This is Lilycoco's website
         </p>
       </JumboTronContainer>
     </JumboTron>
-    <style>
-      {`
-    @media (min-width: 768px) {
-      .jumbotron {
-        padding-top: 6rem;
-        padding-bottom: 6rem;
-      }
-    }
-    .jumbotron p:last-child {
-      margin-bottom: 0;
-    }
-    `}
-    </style>
   </CoverImage>
 )
