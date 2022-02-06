ee445c8f52e9008e82654d0e5129ef45d89f3c35
Ryoko Kawashima <ry.sep27@gmail.com>
2022-02-06 12:55:50 +0900

    add prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import './styles.css'
+import "./styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default App
